0638e999633dc50e55f0ed0c49e885f6371622f7
rvscedgl-ai-ds <rvscedglaiandds@gmail.com>
2026-01-17 13:24:12 +0530

    data.js

--- data.js
+++ data.js
@@ -42,9 +42,10 @@ const datasets = [
     quality: "Unsupervised",
     description: "Synthetic dataset for clustering algorithms.",
     files: {
-      full: "https://rvscedgl-ai-ds.github.io/AI-DS-Academic-Data-Repository/datasets/clustering/AMLB_Academic_cluster.csv"
+      full: "https://rvscedgl-ai-ds.github.io/AI-DS-Academic-Data-Repository/datasets/clustering/AMLB_Academic_Cluster.csv"
     }
   }
 ];
+
 
 
